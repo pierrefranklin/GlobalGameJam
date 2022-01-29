105be765b8da498033e3d353675797001b3cb4b1
Noah <noah@Noahs-MacBook-Pro.local>
2022-01-29 10:24:04 -0500

    Beginning of the player character

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,14 +4,23 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public enum PlayerState { Monster, Platformer};
-    private PlayerState playerState = PlayerState.Platformer;
-    [Header("Movement")]
+    public enum PlayerState { Monster, Platformer, Transforming};
+    public PlayerState playerState = PlayerState.Platformer;
+    [Header("General Physics")]
     private Rigidbody2D myRigidbody;
-    public float maxSpeed = 10f;
-    public float acc = .5f;
-    public float slowDown = .4f;
-    public float jumpSpeed = 7f;
+    public float upwardsGravity = .7f;
+    // the acceleration of gravity when the player is jumping up
+    public float downwardsGravity = 1f;
+    // the acceleration of gravity when the player is falling down
+    [Header("Human Movement")]
+    public float humanMaxSpeed = 10f;
+    public float humanAcc = .5f;
+    public float humanSlowDownAcc = .4f;
+    public float humanJumpSpeed = 7f;
+    [Header("Monster Movement")]
+    public float monstMaxSpeed = 8f;
+    public float monstAcc = .4f;
+    public float monstSlowDownAcc = .3f;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,11 +30,42 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Move();
+        if (playerState == PlayerState.Platformer)
+        {
+            // The code that updates while the player is a platformer
+            AdjustGravity();
+            changeFormStart();
+            Move(humanMaxSpeed, humanAcc, humanSlowDownAcc);
+        }
+        else if (playerState == PlayerState.Monster)
+        {
+            // The code that updates while the player is a monster
+            AdjustGravity();
+            changeFormStart();
+            Move(monstMaxSpeed, monstAcc, monstSlowDownAcc);
+        }
+        else if (playerState == PlayerState.Transforming)
+        {
+            // The player cannot do anything while transforming
+        }
+    }
+
+    // Applies gravity to the player
+    private void AdjustGravity()
+    {
+        Vector2 velocity = myRigidbody.velocity;
+        if (velocity.y > 0)
+        {
+            myRigidbody.gravityScale = upwardsGravity;
+        }
+        else
+        {
+            myRigidbody.gravityScale = downwardsGravity;
+        }
     }
 
     // This will move the player based on the direction held
-    public void Move()
+    public void Move(float maxSpeed, float acc, float slowDown)
     {
         float direction = Input.GetAxis("Horizontal");
         Vector2 velocity = myRigidbody.velocity;
@@ -69,4 +109,26 @@ public class Player : MonoBehaviour
         }
         myRigidbody.velocity = velocity;
     }
+
+    // This will change forms between the monster and the platformer
+    private void changeFormStart()
+    {
+        if (Input.GetKeyDown("k"))
+        {
+            //StartCoroutine(changeForms());
+        }
+    }
+    /*
+    private IEnumerator changeForms()
+    {
+
+        if (playerState == PlayerState.Platformer)
+        {
+            playerState = PlayerState.Monster;
+        }
+        else if (playerState == PlayerState.Monster)
+        {
+            playerState = PlayerState.Platformer;
+        }
+    }*/
 }
